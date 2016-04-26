f2ef1e26b7d7caa2a631f10e535a9f8017b00b21
Blair Yacishyn <b.yacishyn@gmail.com>
2016-04-26 08:17:50 -0600

    added setDivisor function

--- exact_change.js
+++ exact_change.js
@@ -1,3 +1,37 @@
+function setDivisor(denomination) {
+  switch (denomination) {
+    case "ONE HUNDRED":
+      return 100;
+      break;
+    case "TWENTY":
+      return 20;
+      break;
+    case "TEN":
+      return 10;
+      break;
+    case "FIVE":
+      return 5;
+      break;
+    case "ONE":
+      return 1;
+      break;
+    case "QUARTER":
+      return 0.25;
+      break;
+    case "DIME":
+      return 0.1;
+      break;
+    case "NICKEL":
+      return 0.05;
+      break;
+    case "PENNY":
+      return 0.01;
+      break;
+    default:
+      break;
+  }
+}
+
 function checkCashRegister(price, cash, cid) {
   var changeDue;
   changeDue = cash - price;
@@ -14,16 +48,22 @@ function checkCashRegister(price, cash, cid) {
   } else if (changeDue === changeAvailable) {
     console.log("Closed");
     return "Closed";
+  } else {
+    cid.reverse();
+    cid.forEach(function(denomination){
+      var divisor = setDivisor(denomination[0]);
+      // make sure section isn't empty
+      if ((denomination[1] / divisor) > 0) {
+        console.log("Checking the " + divisor + " drawer.");
+      }
+    });
+    console.log("price: $", price.toFixed(2));
+    console.log("cash: $", cash.toFixed(2));
+    console.log("change available: $", changeAvailable.toFixed(2));
+    console.log("change due: $", changeDue.toFixed(2));
+    console.log("cid: ", cid);
+    return changeDue;
   }
-
-  console.log("price: $", price.toFixed(2));
-  console.log("cash: $", cash.toFixed(2));
-  console.log("change available: $", changeAvailable.toFixed(2));
-  console.log("change due: $", changeDue.toFixed(2));
-  console.log("cid: ", cid);
-
-  // Here is your change, ma'am.
-  return changeDue;
 }
 
 // Example cash-in-drawer array:
@@ -37,7 +77,7 @@ function checkCashRegister(price, cash, cid) {
 // ["TWENTY", 60.00],
 // ["ONE HUNDRED", 100.00]]
 
-checkCashRegister(19.50, 20.00, [["PENNY", 0.50], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
+checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
 
 /*
 
