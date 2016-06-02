016761e119506572679ef4e67c9df4017aae3f43
Blair Yacishyn <b.yacishyn@gmail.com>
2016-06-02 15:06:48 -0600

    solved map_the_debris.js

--- map_the_debris.js
+++ map_the_debris.js
@@ -1,7 +1,14 @@
 function orbitalPeriod(arr) {
   var GM = 398600.4418;
   var earthRadius = 6367.4447;
-  return arr;
+  var debris = arr;
+  var orbitalArray = [];
+  debris.forEach(function(piece) {
+    var orbitalPeriod = Math.round((2 * Math.PI) * Math.sqrt(Math.pow(earthRadius + piece.avgAlt, 3)/GM));
+    console.log("name: " + piece.name + "\navgAlt: " + piece.avgAlt + "\norbitalPeriod: " + orbitalPeriod);
+    orbitalArray.push({name: piece.name, orbitalPeriod: orbitalPeriod});
+  });
+  return orbitalArray;
 }
 
 orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
